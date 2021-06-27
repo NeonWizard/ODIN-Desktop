2d3b7dc47e182075aa1624dd0bf3c854cc86b70d
Wes <averagewizard13@gmail.com>
2021-06-27 00:14:01 -0600

    Add skeleton for login page

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div id="login-box">
+      <input id="username-input" v-model="username">
+      <input id="password-input" v-model="password">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+  },
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
