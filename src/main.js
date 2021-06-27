d0d97b1873652026d1935e8d7a2887df9f904832
Wes <averagewizard13@gmail.com>
2021-06-27 03:31:24 -0600

    Fix some broken elementui styling

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/reset.css'
 import elementLocale from 'element-ui/lib/locale/lang/en'
 
 import App from '@/App.vue'
