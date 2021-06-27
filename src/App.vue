343de6091f3fb2d56f17b71a953f70690ead1dbf
Wes <averagewizard13@gmail.com>
2021-06-26 23:33:23 -0600

    Dark themeify hehe yea boy

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,15 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #181818;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #c2c2c2;
   margin-top: 60px;
 }
 </style>
